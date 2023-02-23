445f1bb4d209dc61982b73db3cb2a24ba8496d24
hesperhu <hesper.hu@gmail.com>
2023-02-23 14:58:52 +0800

    Initial Commit

---
+++ MenuBar/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MenuBar
+//
+//  Created by hesper on 2023-02-23(Thu).
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MenuBar/MenuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuBarApp.swift
+//  MenuBar
+//
+//  Created by hesper on 2023-02-23(Thu).
+//
+
+import SwiftUI
+
+@main
+struct MenuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
